724d60e6ef9fa28fc97bb0a584a3c559d57b724d
yashika0810 <khatri.yashika48@gmail.com>
2019-05-01 06:54:40 -0700

    multiplying two numbers

---
+++ multiply.ts
@@ -0,0 +1,17 @@
+class Demo{
+    x:number;
+    y:number;
+    multiply(x,y)
+{
+  
+   
+    this.x=x*2
+    console.log('value of x is'+x);
+   
+    this.y=y*2
+    console.log('value of y is'+y);
+console.log('x is'+this.x + 'y is'+this.y)
+}
+}
+let point:Demo=new Demo();
+point.multiply(2,3);
