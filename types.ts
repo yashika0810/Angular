009227f54bf41e447f588250da623a962e872c81
yashika0810 <khatri.yashika48@gmail.com>
2019-05-01 06:45:40 -0700

    This is new version

---
+++ types.ts
@@ -0,0 +1,13 @@
+let a:number;
+a=1;
+let b:boolean;
+let c:string;
+let e:number[] = [1,2,3]
+let f:any[]=[1,true]
+
+const ColorRed=0;
+const ColorBlue=1;
+const ColorGreen=2;
+console.log(a);
+enum Color {Red=6,Blue,Green};//enum is a container in which we can store related constants//
+let backgroundColor = Color.Red;//background is a variable//enum member
