c3a709eced4aac83ca689b2ab298420ce4d7e87b
yashika0810 <khatri.yashika48@gmail.com>
2019-05-02 05:47:55 -0700

    new changes

--- arrow.ts
+++ arrow.ts
@@ -2,7 +2,7 @@ let demo=function(message){
     console.log(message);
 }
 //in javascript
-let demo1=(message) =>{
-    console.log(message)
+let demo1=(value) =>{
+    console.log(value)
 }
 //in typescript
